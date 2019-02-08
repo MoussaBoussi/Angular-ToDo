c8a294812b28f9cc238ee290e2b4b20a4dd0d690
Moussa Boussi <Moussaboussi@gmail.com>
2019-02-07 22:43:07 -0500

    Initial commit

---
+++ app.controller.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function TodoFunction() {
+  const vm = this;
+  vm.todos = [];
+  
+
+  vm.add = function(newTodo) {
+    newTodo.isComplete = false
+    vm.todos.push(angular.copy(newTodo))
+    console.log(vm.todos)
+    
+    vm.newTodo = {}
+  
+  }
+  vm.delete = function(index) {
+    vm.todos.splice(index, 1);
+  }
+}
+
+angular
+  .module("todoApp")
+  //Building a controller. 
+  .controller("TodoController", TodoFunction);
