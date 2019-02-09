e266d98c776f996422439eedfa830c726d2d4304
Moussa <moussaboussi@gmail.com>
2019-02-09 13:58:17 -0500

    added comments

--- app.controller.js
+++ app.controller.js
@@ -11,26 +11,35 @@ function TodoFunction() {
   
 
   vm.add = function(newTodo) {
+    // Gives every new object a property of isComplete that is false
     newTodo.isComplete = false
+    // pushes the newTodo Object into the aray of todos and makes a copy of each
+    // angular.copy prevents the array from making duplicates of the new object pushed
     vm.todos.push(angular.copy(newTodo))
-    
+    // emptys the object newTodo so that the add button can be disabled when its empty
     vm.newTodo = {}
-  
   }
+
   vm.removeTask = function(index) {
+    // splices todos at the index passed from html
     vm.todos.splice(index, 1);
   }
+
   vm.completeTask = function(index) {
+    // sets the property isComplete = to true at the passed index
     vm.todos[index].isComplete = true
   }
 
   vm.updateText = function(index, e) {
+    // sets the task property of the selected index to equal the text content of the target element
+    // :)
     vm.todos[index].task = e.target.textContent;
   }
 }
 
 
 angular
+  // links module
   .module("todoApp")
   //Building a controller. 
   .controller("TodoController", TodoFunction);
--- app.module.js
+++ app.module.js
@@ -1,3 +1,4 @@
 "use strict";
 
+// creates module and lists dependencies, in this case there are none.
 angular.module("todoApp", []);
